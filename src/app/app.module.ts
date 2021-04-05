894398813356b296231abd34aa6bae176f722e72
Olha Yegorova <OYEGOROVA@pier1.com>
2021-04-06 00:04:54 +0300

    routing with login and dashboard pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    LoginComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
