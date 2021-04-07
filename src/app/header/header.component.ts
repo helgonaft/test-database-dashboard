b6b71a913a54f1b8b2d456e1955ab120e5becd02
Olha Yegorova <OYEGOROVA@pier1.com>
2021-04-07 16:08:00 +0300

    redirect to login after logout

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-header",
@@ -8,7 +9,12 @@ import { Component, OnInit } from "@angular/core";
 export class HeaderComponent implements OnInit {
   public username: string = "User";
 
-  constructor() {}
+  logout(): void {
+    // TODO: logout user and redirect to login page
+    this.router.navigate(["login"]);
+  }
+
+  constructor(private router: Router) {}
 
   ngOnInit() {}
 }
