b5c8df1f0ea067d217ea796c653c6aab85525aa7
Olha Yegorova <OYEGOROVA@pier1.com>
2021-04-10 17:59:30 +0300

    cluster sizes constant

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,1 +1,8 @@
-export const CLUSTER_SIZES = ["m5.large", "m5.xlarge", "m5.2xlarge"];
+export const CLUSTER_SIZES = [
+  "m5.large",
+  "m5.xlarge",
+  "m5.2xlarge",
+  "m5.4xlarge",
+  "m5.12xlarge",
+  "m5.24xlarge"
+];
