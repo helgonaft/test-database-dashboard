c4e2c9be3c125b686fdc4825ef44b48eb2e15545
Olha Yegorova <OYEGOROVA@pier1.com>
2021-04-10 23:25:55 +0300

    start and stop main cluster

--- src/app/dashboard/cluster/cluster.component.ts
+++ src/app/dashboard/cluster/cluster.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit, Input } from "@angular/core";
 import { AuthService } from "../../services/authentication.service";
 import { Cluster } from "src/app/types";
+import {
+  startDatabaseAndMainCluster,
+  stopDatabaseAndAllClusters,
+  startWorkerCluster
+} from "src/app/actions/database.actions";
+import { Store } from "@ngrx/store";
+import { AppState } from "src/app/reducers";
 
 @Component({
   selector: "app-cluster",
@@ -11,11 +18,30 @@ export class ClusterComponent implements OnInit {
   @Input() cluster: Cluster;
   @Input() isMain: boolean;
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private store: Store<AppState>
+  ) {}
 
   ngOnInit() {}
 
   get isAdmin() {
     return this.authService.isAdmin;
+  }
+
+  startCluster(cluster: Cluster) {
+    if (this.isMain) {
+      this.store.dispatch(startDatabaseAndMainCluster());
+    } else {
+      this.store.dispatch(startWorkerCluster(cluster));
+    }
+  }
+
+  stopCluster(cluster: Cluster) {
+    if (this.isMain) {
+      this.store.dispatch(stopDatabaseAndAllClusters());
+    } else {
+      this.store.dispatch(stoptWorkerCluster(cluster));
+    }
   }
 }
